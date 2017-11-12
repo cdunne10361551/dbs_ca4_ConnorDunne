9480c06dcf440cdf96415bc9b99fb84b410661d0
cdunne10361551 <32909729+cdunne10361551@users.noreply.github.com>
2017-11-12 21:51:15 +0000

    Add files via upload

--- ConnorDunne_10361551_CA4.py
+++ ConnorDunne_10361551_CA4.py
@@ -1,4 +1,6 @@
 import pandas as pd
+import numpy as np
+from pandas import Series, DataFrame, Panel
 
 def read_file(changes_file):
     # use strip to strip out spaces and trim the line.
@@ -7,22 +9,46 @@ def read_file(changes_file):
 
 def get_commits(data):
     sep = 72*'-'
+    #new_sep = "Changed paths:"
     commits = []
     #current_commit = None
     index = 0
     while index < len(data):
         try:
+            #get counts of actions
+            new_index = index + 3
+            count_a = 0
+            count_d = 0
+            count_m = 0
+            result = 1
+            while result != 0:
+                value = data[new_index][0]
+                if value == "A":
+                    count_a = count_a + 1
+                elif value == "D":
+                    count_d = count_d + 1
+                elif value == "M":
+                    count_m = count_m + 1
+                new_index = new_index + 1   
+                checker = data[new_index]
+                result = len(checker)
+                
             # parse each of the commits and put them into a list of commits
             details = data[index + 1].split('|')
             # the author with spaces at end removed.
             commit = {'revision': details[0].strip(),
                 'author': details[1].strip(),
                 'date': details[2].strip(),
-                'number_of_lines': details[3].strip().split(' ')[0]
+                'number_of_lines': details[3].strip().split(' ')[0],
+                "Add_Counts": count_a,
+                "Delete_Counts": count_d,
+                "Modify_Counts": count_m
             }
+    
             # add details to the list of commits.
             commits.append(commit)
-             #use index to locate sep position and start on next line
+            
+            #use index to locate sep position and start on next line   
             index = data.index(sep, index + 1)
         except IndexError:
             break
@@ -30,15 +56,39 @@ def get_commits(data):
     df = pd.DataFrame(data = commits)
     return df
 
+def clean_dates(dates):
+    start_ind = "+"
+    d_fixed = []
+    for i in range(len(dates)):
+        index = dates[i].index(start_ind)
+        d_clean = dates[i][0:index-1]
+        d_fixed.append(d_clean)
+    return d_fixed
+    
 if __name__ == '__main__':
     # open the file - and read all of the lines.
     changes_file = 'changes_python.log'
     data = read_file(changes_file)
     commits = get_commits(data)
+    d_fixed = clean_dates(commits["date"])
+    commits["date"] = d_fixed
 
+    commits["date"]
     # print the number of lines read
     ##print(len(data))
     #print(commits)
     ##print(commits[0])
     ##print(commits[1]['author'])
     ##print(len(commits))
+    #type(commits_sorted["Add_Counts"][0])
+    
+    #analysis
+    commits['date'] = commits['date'].astype('datetime64[ns]')
+    commits["Add_Counts"] = pd.to_numeric(commits["Add_Counts"])
+    pd.to_numeric(commits["Delete_Counts"])
+    pd.to_numeric(commits["Modify_Counts"])
+    commits_sorted = commits.sort_values(by = "date")
+    dates = pd.date_range('2015-07', '2015-12', freq='D')
+    AO = Series(commits_sorted["Add_Counts"], index=commits_sorted["date"])
+    commits.plot(commits_sorted["Add_Counts"],commits_sorted["date"])
+    AO = Series(commits_sorted["date"],index =commits_sorted["Add_Counts"])
